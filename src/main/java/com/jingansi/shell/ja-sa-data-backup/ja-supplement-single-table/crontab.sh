c0cb14e6d0d7cf9cbda6cfb0051c526ccf0d9dc4
丁串串 <1577940994@qq.com>
2024-09-09 22:45:26 +0800

    fix:supplement-data

--- src/main/java/com/jingansi/shell/ja-sa-data-backup/ja-supplement-single-table/crontab.sh
+++ src/main/java/com/jingansi/shell/ja-sa-data-backup/ja-supplement-single-table/crontab.sh
@@ -4,7 +4,7 @@
 
 
 #第一种不传入参数 start-v1.sh
-#第二种传入参数  start-v2.sh
+#第二种传入参数  start_supp_table.sh
 #  start_day、end_day、table_name、time_column、
 
 sh start.sh "2024-06-22 13:00:00" "2024-06-22 13:30:00" "dws_aircraft_combine_list_rt" "acquire_time"
