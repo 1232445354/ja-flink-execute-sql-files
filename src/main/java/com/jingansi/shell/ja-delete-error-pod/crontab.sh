0ec775d34ea910ce5afd6447482c4393260bf116
丁串串 <1577940994@qq.com>
2024-12-10 11:01:43 +0800

    fix:ja-delete-error-pod

--- src/main/java/com/jingansi/shell/ja-delete-error-pod/crontab.sh
+++ src/main/java/com/jingansi/shell/ja-delete-error-pod/crontab.sh
@@ -1,4 +1,4 @@
 
 
 mkdir -r /data1/bigdata/apps/ja-delete-error-pod
-1 * * * * sh /data1/bigdata/apps/ja-delete-error-pod/run.sh > /data1/bigdata/apps/ja-delete-error-pod/root.log &
+*/10 * * * * sh /data1/bigdata/apps/ja-delete-error-pod/run.sh > /data1/bigdata/apps/ja-delete-error-pod/root.log
