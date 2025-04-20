12be4ca247fff6592f777b5a0338cd0770b5f469
丁串串 <1577940994@qq.com>
2025-04-20 15:45:48 +0800

    fix:同步数据路径

--- src/main/java/com/jingansi/shell/jinghang-sa-shell/ja-sync-crontab-data/ja-sync-data/crontab.sh
+++ src/main/java/com/jingansi/shell/jinghang-sa-shell/ja-sync-crontab-data/ja-sync-data/crontab.sh
@@ -2,5 +2,5 @@
 
 # 部署警航的135.100.11.132 服务器
 
-10 2 * * * nohup sudo sh /data1/bigdata/apps/sync-jinghang/ja-sync-data/start.sh  > /data1/bigdata/apps/sync-jinghang/ja-sync-data/root.log 2>&1 &
+10 2 * * * nohup sudo sh /data1/bigdata/apps/ja-sync-crontab-data/ja-sync-data/start.sh  > /data1/bigdata/apps/ja-sync-crontab-data/ja-sync-data/root.log 2>&1 &
 
