2968eade96980a38892bf4a806ba55eaa3e9a367
丁串串 <1577940994@qq.com>
2025-05-23 10:51:37 +0800

    fix:多jar上传minio

--- src/main/java/com/jingansi/shell/ja/bigdata-apps/put-minio-file/put-minio-file.sh
+++ src/main/java/com/jingansi/shell/ja/bigdata-apps/put-minio-file/put-minio-file.sh
@@ -1,36 +1,54 @@
 #!/bin/bash
 
-DIR=$(cd `dirname $0`; pwd) # /home/jingansi/k8s/init/bigdata-apps
+DIR=$(cd `dirname $0`; pwd)       # /home/jingansi/k8s/init/bigdata-apps
 echo "当前文件目录：${DIR}"
 
-# 设置相关变量
-JAR_FILE_PATH="${DIR}/geo-udf-1.0-SNAPSHOT-jar-with-dependencies.jar" # 宿主机上的 .jar 文件路径
-CONTAINER_NAME="minio-0"            # MinIO 容器的名称或ID
-CONTAINER_DEST_PATH="/opt/bitnami/minio-client/bin/geo-udf-1.0-SNAPSHOT-jar-with-dependencies.jar"  # 容器内存放文件的路径
-ALIAS="myminio"                     # mc 配置中的 MinIO 服务别名
-MINIO_URL="http://minio.minio.svc.cluster.local:9000"     # MinIO 服务的 URL
-ACCESS_KEY="minio"                                # MinIO 访问密钥
-SECRET_KEY="jingansi110"                          # MinIO 私密密钥
-BUCKET_NAME="ja-resource"                         # MinIO 存储桶名称
-OBJECT_PATH="java/20241218/"
+# 1.获取minio的容器名称
+CONTAINER_NAME=$(kubectl get pod -n minio -o name | awk -F/ '{print $2}')
+echo "获取到minio的容器名称：${CONTAINER_NAME}"   # MinIO 容器的名称或ID
 
+# 2.设置相关变量
+CONTAINER_PRE_PATH="/opt/bitnami/minio-client/bin"    # 容器内存放文件的路径,先上传
+ALIAS="myminio"                                       # mc 配置中的 MinIO 服务别名
+MINIO_URL="http://minio.minio.svc.cluster.local:9000" # MinIO 服务的 URL
+ACCESS_KEY="minio"                                    # MinIO 访问密钥
+SECRET_KEY="jingansi110"                              # MinIO 私密密钥
+BUCKET_NAME="ja-resource"                             # MinIO 存储桶名称
 
-# 1、将 .jar 文件从宿主机复制到 MinIO 容器内
-echo "Copying ${JAR_FILE_PATH} to MinIO container..."
-kubectl -n minio cp ${JAR_FILE_PATH} ${CONTAINER_NAME}:${CONTAINER_DEST_PATH}
+# 3.需要上传的jar
+JAR_FILES=(
+"geo-udf-1.0-SNAPSHOT-jar-with-dependencies.jar java/20241218"
+"mysql-connector-java-8.0.25.jar java/20250312"
+)
 
-# 2、创建mc和minio的连接
+# 4.创建minio容器内mc的连接
 echo "Setting up MinIO connection in the container..."
 kubectl exec -it "${CONTAINER_NAME}" -nminio -- sh -c " mc alias set "${ALIAS}" "${MINIO_URL}" "${ACCESS_KEY}" "${SECRET_KEY}" "
 
+# 5.遍历jar数组上传
+for JAR_INFO in "${JAR_FILES[@]}"; do
+  # 使用 awk 或 read 分割数组元素（文件名 和 MinIO路径）
+  JAR_FILE=$(echo "$JAR_INFO" | awk '{print $1}')
+  OBJECT_PATH=$(echo "$JAR_INFO" | awk '{print $2}')
+  echo "Processing ${JAR_FILE} -> MinIO Path: ${OBJECT_PATH}..."
 
-#3、在容器内运行 mc 命令上传文件到 MinIO
-echo "Uploading ${CONTAINER_DEST_PATH} to MinIO bucket ${BUCKET_NAME}..."
-kubectl exec -it "${CONTAINER_NAME}" -nminio -- sh -c " mc cp "${CONTAINER_DEST_PATH}" "${ALIAS}/${BUCKET_NAME}/${OBJECT_PATH}" "
+  JAR_FILE_PATH="${DIR}/${JAR_FILE}"
+  CONTAINER_DEST_PATH="${CONTAINER_PRE_PATH}/${JAR_FILE}"
 
-#4、删除容器内文件
-echo "Cleaning up container's file..."
-kubectl exec -it "${CONTAINER_NAME}" -nminio -- sh -c " rm -rf ${CONTAINER_DEST_PATH} "
+  echo "Copying ${JAR_FILE_PATH} to MinIO container..."
+  kubectl -n minio cp ${JAR_FILE_PATH} ${CONTAINER_NAME}:${CONTAINER_DEST_PATH}
 
+  echo "Uploading ${JAR_FILE} to MinIO bucket ${BUCKET_NAME}/${OBJECT_PATH}..."
+  kubectl exec -it "${CONTAINER_NAME}" -n minio -- sh -c "mc cp ${CONTAINER_DEST_PATH} ${ALIAS}/${BUCKET_NAME}/${OBJECT_PATH}/"
+
+  echo "Cleaning up container's file..."
+  kubectl exec -it "${CONTAINER_NAME}" -nminio -- sh -c " rm -rf ${CONTAINER_DEST_PATH} "
+
+  echo "------------------------------------------------------"
+done
+
+echo -en "================================================================\n"
 echo "File upload completed!"
+echo -en "================================================================\n"
+
 
