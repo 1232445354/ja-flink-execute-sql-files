ac79c24f47ec3faf116dc627b88fde8768cfa3ba
丁串串 <1577940994@qq.com>
2025-06-17 16:53:42 +0800

    fix:更新注释

--- src/main/java/com/jingansi/dushu-wanglou/ja-chingchi-icos3/0-rt/靖安版本/crontab.sh
+++ src/main/java/com/jingansi/dushu-wanglou/ja-chingchi-icos3/0-rt/靖安版本/crontab.sh
@@ -4,6 +4,6 @@
 3. ja-chingchi-icos3.0-rt-v250522 新增拍照数据经纬度
 4. ja-chingchi-icos3.0-rt-v250609 天朗雷达新增入库字段 和上个版本拍照经纬度没有合并代码呢
 5. ja-chingchi-icos3.0-rt-v250612 无人机检测目标入库 合并天朗雷达新增字段及其拍照经纬度
-6. ja-chingchi-icos3.0-rt-v250617 拍照数据切换topic,接入设备服务 事件数据
+6. ja-chingchi-icos3.0-rt-v250617 拍照数据切换topic
 
 
