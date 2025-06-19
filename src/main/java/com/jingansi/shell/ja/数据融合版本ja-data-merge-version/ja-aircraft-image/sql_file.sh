8f79c9fc2530a10e4643ad28037267b1c90a6afd
丁串串 <1577940994@qq.com>
2025-06-19 18:03:22 +0800

    fix:飞机图片聚合

--- src/main/java/com/jingansi/shell/ja/数据融合版本ja-data-merge-version/ja-aircraft-image/sql_file.sh
+++ src/main/java/com/jingansi/shell/ja/数据融合版本ja-data-merge-version/ja-aircraft-image/sql_file.sh
@@ -26,36 +26,43 @@ group by flight_photo,flight_id;
 
 insert into sa.dws_atr_aircraft_image_info
 select
-	flight_id,
-	src_code,
-	acquire_time,
-	image_id,
-	image_url,
-	remark,
-	create_by,
-	update_time
+  t1.*
 from (
-select
-	t2.flight_id,
-	1 as src_code,
-	t2.acquire_time,
-	t1.flight_photo as image_id,
-	t1.minio_url as image_url,
-	null as remark,
-	null as create_by,
-	t1.update_time,
-	row_number()over(partition by t2.flight_id order by length(t1.minio_url) desc ) as rk
-from (
-select * from sa.dws_aircraft_image_info
-where update_time between days_sub(date_trunc('${start_time}','day'),3) and date_trunc('${start_time}','day')
- ) as t1
-inner join (
-	select * from sa.dws_atr_aircraft_image_id_info
-	where update_time between days_sub(date_trunc('${start_time}','day'),3) and date_trunc('${start_time}','day')
- ) as t2
-on t1.flight_photo = t2.image_id
+  select
+  	flight_id,
+  	src_code,
+  	acquire_time,
+  	image_id,
+  	image_url,
+  	remark,
+  	create_by,
+  	update_time
+  from (
+  select
+  	t2.flight_id,
+  	1 as src_code,
+  	t2.acquire_time,
+  	t1.flight_photo as image_id,
+  	t1.minio_url as image_url,
+  	null as remark,
+  	null as create_by,
+  	t1.update_time,
+  	row_number()over(partition by t2.flight_id order by length(t1.minio_url) desc ) as rk
+  from (
+  select * from sa.dws_aircraft_image_info
+  where update_time between days_sub(date_trunc('${start_time}','day'),3) and date_trunc('${start_time}','day')
+   ) as t1
+  inner join (
+  	select * from sa.dws_atr_aircraft_image_id_info
+  	where update_time between days_sub(date_trunc('${start_time}','day'),3) and date_trunc('${start_time}','day')
+   ) as t2
+  on t1.flight_photo = t2.image_id
+  ) as t1
+  where rk = 1
 ) as t1
-where rk = 1
+left join sa.dws_atr_aircraft_image_info as t2
+on t1.flight_id = t2.flight_id
+where t2.remark is null
 
 "
 
--- src/main/java/com/jingansi/shell/ja/数据融合版本ja-data-merge-version/ja-aircraft-image/sql_file_ddl.sql
+++ src/main/java/com/jingansi/shell/ja/数据融合版本ja-data-merge-version/ja-aircraft-image/sql_file_ddl.sql
@@ -18,31 +18,35 @@
 
 insert into sa.dws_atr_aircraft_image_info
 select
-    flight_id,
-    src_code,
-    acquire_time,
-    image_id,
-    image_url,
-    remark,
-    create_by,
-    update_time
+    t1.*
 from (
-         select
-             t2.flight_id,
-             1 as src_code,
-             t2.acquire_time,
-             t1.flight_photo as image_id,
-             t1.minio_url as image_url,
-             null as remark,
-             null as create_by,
-             t1.update_time,
-             row_number()over(partition by t2.flight_id order by length(t1.minio_url) desc ) as rk
-         from dws_aircraft_image_info as t1
-                  inner join dws_atr_aircraft_image_id_info as t2
-                             on t1.flight_photo = t2.image_id
--- where t2.flight_id = 'AE08E3'
---              limit 100
-     ) as t1
-where rk = 1
-
+    select
+        flight_id,
+             src_code,
+             acquire_time,
+             image_id,
+             image_url,
+             remark,
+             create_by,
+             update_time
+      from (select t2.flight_id,
+                   1               as src_code,
+                   t2.acquire_time,
+                   t1.flight_photo as image_id,
+                   t1.minio_url    as image_url,
+                   null            as remark,
+                   null            as create_by,
+                   t1.update_time,
+                   row_number()       over(partition by t2.flight_id order by length(t1.minio_url) desc ) as rk
+            from dws_aircraft_image_info as t1
+                     inner join dws_atr_aircraft_image_id_info as t2
+                                on t1.flight_photo = t2.image_id
+               -- where t2.flight_id = 'AE08E3'
+               --              limit 100
+           ) as t1
+      where rk = 1
+) as t1
+left join sa.dws_atr_aircraft_image_info as t2
+on t1.flight_id = t2.flight_id
+where t2,remark is null
 
