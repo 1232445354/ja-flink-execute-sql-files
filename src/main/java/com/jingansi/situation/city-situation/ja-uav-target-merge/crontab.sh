de677a81e2d98b6ac220e1ab1939b0aecece739e
丁串串 <1577940994@qq.com>
2025-07-03 17:46:54 +0800

    fix:无人机融合

---
+++ src/main/java/com/jingansi/situation/city-situation/ja-uav-target-merge/crontab.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+1. ja-rid-data-rt-v250215       第一版只接入RID
+2. ja-uav-target-merge-v250428  rid和aoa数据融合,直接关联写法 给警航
+3. ja-uav-target-merge-v250511  优化融合逻辑
+4. ja-uav-target-merge-v250703  雷莫雷达接入
+
+
+
