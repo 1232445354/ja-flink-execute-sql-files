99a72819ff45e466870de6d9602c8652edcb6ebf
dahjtue <yibo@jingan-inc.com>
2025-09-28 15:07:06 +0800

    fix:init

--- src/main/java/com/jingansi/program/dushu-wanglou/ja-chingchi-icos3/0-rt/xiaoshan/ja-chingchi-icos3.0-rt-v250923.sql
+++ src/main/java/com/jingansi/program/dushu-wanglou/ja-chingchi-icos3/0-rt/xiaoshan/ja-chingchi-icos3.0-rt-v250923.sql
@@ -1002,6 +1002,20 @@ select
 from tmp_track_01;
 
 
+-- 截图 & 拍照 & 视频 不是fh_sync开头的 直接入kafka open
+insert into file_upload_callback_sink_kafka
+select
+    'FILE_UPLOAD_CALLBACK' as event,
+    UNIX_TIMESTAMP(cast(start_time as varchar),'yyyy-MM-dd HH:mm:ss') * 1000 as `time`,
+    get_row_udf(
+            type,
+            if (`method` in ('platform.capture.post','event.videoMediaFileUpload.info'),concat('/',url),url),
+            extends
+        ) as data1
+from tmp_image_01
+where `method` = 'platform.capture.post'  -- 截图 或者 （拍照 &视频）不是fh_sync开头的
+   or instr(url,'fh_sync') = 0;
+
 
 -- 截图 & 拍照 & 视频 不是fh_sync开头的 直接入库msyql
 insert into device_media_datasource
