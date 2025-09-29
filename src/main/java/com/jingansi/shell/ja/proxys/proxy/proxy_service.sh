d914ab17ee4b4b1ad0792622ce7c611042d57dd2
dahjtue <yibo@jingan-inc.com>
2025-09-29 19:27:49 +0800

    fix:init

--- src/main/java/com/jingansi/shell/ja/proxys/proxy/proxy_service.sh
+++ src/main/java/com/jingansi/shell/ja/proxys/proxy/proxy_service.sh
@@ -3,7 +3,4 @@ source /data1/apps/sa_base/venv/bin/activate
 
 echo -e "\n\nProxy service $(date '+%Y-%m-%d %H:%M:%S')..." >> logs/proxy_service.log
 echo " Active python venv ..." >> logs/proxy_service.log
-./proxy_fr24.sh >> logs/proxy_service.log 2>&1
-./proxy_adsb.sh >> logs/proxy_service.log 2>&1
-./proxy_radarbox.sh >> logs/proxy_service.log 2>&1
 ./proxy_radarbox_area.sh >> logs/proxy_service.log 2>&1
